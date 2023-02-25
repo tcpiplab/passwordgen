ccc794c85a843869fbc7fad3cabf63bf90f7417b
tcpiplab <lshep.usc@gmail.com>
2023-02-25 11:51:07 -0800

    final splits, update README.md

--- README.md
+++ README.md
@@ -13,22 +13,44 @@ $ go build passwordgen.go
 ## Run
 
 ```shell
-$ ./passwordgen 12
-6e7BDqfZ6SfK
-XZy33a9GGl*I
-TaQd32(N6CHI
-0Hr#t!v@LZXe
-k0Ywcgi2B(al
-78THj%APPSkJ
-heightAndWidthString*Uq34fLH%4#
-42O40r6YNuT7
-aizZ7oWI9Dl7
-sgHQFn!T7aWO
-P1WnusUuJZOz
-)TnhUXswU5p5
-gFuqmEmWEZ0#
-PDp9bl3&%oX^
-bbw&x7Hjc@!a
-olIgFO%Fh*So
-433#lZV)bxvX
+$ ./passwordgen 20
++────+──────────────────────+
+│ 00 │ l[TB(15k&6H3RU55iC11 │
++────+──────────────────────+
+│ 01 │ Mt6Zy0Q)s@&V3w2o#0aA │
++────+──────────────────────+
+│ 02 │ xkTKxfRpGAfHUTCxEqZB │
++────+──────────────────────+
+│ 03 │ @DJvj!jGgG5w8uwwGtS1 │
++────+──────────────────────+
+│ 04 │ {EpREgvQguP[i]7!x2OV │
++────+──────────────────────+
+│ 05 │ S%cKxii2@01r1cHt8^(k │
++────+──────────────────────+
+│ 06 │ ^dAK33@0NS(OWepX*#u7 │
++────+──────────────────────+
+│ 07 │ 8H^8m&ifDl9KghtaymKx │
++────+──────────────────────+
+│ 08 │ [dkq2#Qu6g44FdBt8f@D │
++────+──────────────────────+
+Enter an integer: 5
+Input has been copied to clipboard.
+Waiting for 60 seconds before clearing the clipboard.
+█████████████████████████████████████████████████████████████60
+```
 
+# Usage
+
+```shell
+  -erase
+        ./passwordgen -erase[=false]
+         (default true)
+  -help
+        ./passwordgen n
+        Where n is the length of the password.
+        Length must be the last argument.
+  -interactive
+        ./passwordgen -interactive[=false]
+         (default true)
+         Note that `false` behavior is not yet implemented.
+```
---
+++ handleInputs.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/fatih/color"
+	"os"
+)
+
+func argsHandler() (*bool, *bool, bool) {
+	help := flag.Bool(
+		"help",
+		false,
+		"./passwordgen n\nWhere n is the length of the password.\nLength must be the last argument.",
+	)
+	//flag.Parse()
+
+	if *help {
+		flag.Usage()
+		return nil, nil, true
+	}
+
+	// Interactive mode is the default
+	interactive := flag.Bool(
+		"interactive",
+		true,
+		"./passwordgen -interactive[=false]\n")
+
+	erase := flag.Bool(
+		"erase",
+		true,
+		"./passwordgen -erase[=false]\n")
+
+	flag.Parse()
+
+	// For now the length is mandatory and must be the last arg
+	if len(os.Args) < 2 {
+
+		color.HiRed("\nPlease provide a password length as an argument\nOr -h for help.\n")
+		return nil, nil, true
+	}
+	return interactive, erase, false
+}
+
+func ifInteractive(interactive *bool, rows int) bool {
+
+	// TODO: handle `--interactive=false` arg by returning one password and exiting
+	if *interactive {
+
+		// Declare a variable to store the user's choice of which password they select
+		var passwordNumber int
+
+		// Prompt the user to choose a password from the list
+		fmt.Print("Enter an integer: ")
+
+		for {
+			// Accept user input and save it to passwordNumber
+			// We don't need the number of args, which is the first returned value,
+			// so just put that in '_'
+			_, err := fmt.Scan(&passwordNumber)
+
+			// Check if input is an integer. If not, re-prompt the user
+			if err != nil {
+
+				fmt.Printf("Error: Expected input to be an integer: %s", err)
+				fmt.Printf("\nEnter an integer: ")
+				continue
+			}
+
+			// Check if selected password number is in range
+			if passwordNumber < 0 || passwordNumber >= (rows-1) {
+
+				fmt.Printf("Error: Your selection is out of range")
+				fmt.Printf("\nEnter an integer: ")
+				continue
+			}
+			break
+
+		}
+
+		// Set the global var to the entered number
+		selectedPasswordNumber = passwordNumber
+
+		return true
+	}
+	return false
+}
