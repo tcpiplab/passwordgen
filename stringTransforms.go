96a4b98b52c520f0f23caceec857c13374e51e1b
tcpiplab <lshep.usc@gmail.com>
2023-02-28 09:02:08 -0800

    Improve transforms. Some run less often now.

--- stringTransforms.go
+++ stringTransforms.go
@@ -101,45 +101,74 @@ func matchCharacterPair(leftChar rune, rightChar rune) rune {
 
 func surroundString(input string) string {
 
-	// Initialize a slice containing the valid surrounding characters
-	chars := []rune{'_', '-', '*', '&', '^', '%', '$', '#', '@', '!', '~', '?', '.'}
-
-	// Call the surroundString function with an input string
-	//input := "hello"
-
-	// Seed the random number generator
+	// Only run this transformation routine about 25% of the times it is called
+	// Generate a random number between 0 and 3
 	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(4)
 
-	// Randomly select a character from the slice of valid characters
-	randChar := chars[rand.Intn(len(chars))]
+	// Only perform the routines if the random number is 0
+	if randomNumber == 0 {
 
-	// Surround the input string with the randomly selected character
-	output := string(randChar) + input + string(randChar)
+		// Initialize a slice containing the valid surrounding characters
+		chars := []rune{'_', '-', '*', '&', '^', '%', '$', '#', '@', '!', '~', '?', '.'}
 
-	//fmt.Printf("Input string: %s\nOutput string: %s\n", input, output)
+		// Call the surroundString function with an input string
+		//input := "hello"
 
-	return output
+		// Seed the random number generator
+		rand.Seed(time.Now().UnixNano())
+
+		// Randomly select a character from the slice of valid characters
+		randChar := chars[rand.Intn(len(chars))]
+
+		// Surround the input string with the randomly selected character
+		output := string(randChar) + input + string(randChar)
+
+		//fmt.Printf("Input string: %s\nOutput string: %s\n", input, output)
+
+		return output
+
+	} else {
+
+		return input
+	}
+
 }
 
 func randomCase(input string) string {
-	// Seed the random number generator
+
+	// Generate a random number between 0 and 2
 	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(3)
 
-	// Convert the input string to a slice of runes
-	inputRunes := []rune(input)
+	// Only perform the routines about 33% of the times that this function
+	// is called, i.e., if the random number is 0
+	if randomNumber == 0 {
 
-	// Iterate over the slice of runes and randomly change the case of each letter
-	for i := 0; i < len(inputRunes); i++ {
-		if rand.Intn(2) == 0 {
-			inputRunes[i] = []rune(strings.ToUpper(string(inputRunes[i])))[0]
-		} else {
-			inputRunes[i] = []rune(strings.ToLower(string(inputRunes[i])))[0]
+		// Seed the random number generator
+		rand.Seed(time.Now().UnixNano())
+
+		// Convert the input string to a slice of runes
+		inputRunes := []rune(input)
+
+		// Iterate over the slice of runes and randomly change the case of each letter
+		for i := 0; i < len(inputRunes); i++ {
+			if rand.Intn(2) == 0 {
+				inputRunes[i] = []rune(strings.ToUpper(string(inputRunes[i])))[0]
+			} else {
+				inputRunes[i] = []rune(strings.ToLower(string(inputRunes[i])))[0]
+			}
 		}
-	}
 
-	// Convert the slice of runes back to a string and return it
-	output := string(inputRunes)
-	return output
+		// Convert the slice of runes back to a string and return it
+		output := string(inputRunes)
+
+		return output
+
+	} else {
+
+		return input
+	}
 }
 
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
@@ -165,10 +194,28 @@ func randomString(length int) string {
 //	println("Input string:", input)
 //	println("Output string:", output)
 
+//func randPadString(input string) string {
+//	// Determine the number of characters to pad
+//	//numPadding := length - len(input)
+//
+//	numPadding := requestedPasswordLength - len(input)
+//
+//	// If the input string is already longer than the desired length, return the input string
+//	if numPadding <= 0 {
+//		return input
+//	}
+//
+//	// Generate random strings for the left and right padding
+//	leftPadding := randomString(numPadding / 2)
+//	rightPadding := randomString(numPadding - len(leftPadding))
+//
+//	// Concatenate the left and right padding with the input string and return the result
+//	output := leftPadding + input + rightPadding
+//	return output
+//}
+
 func randPadString(input string) string {
 	// Determine the number of characters to pad
-	//numPadding := length - len(input)
-
 	numPadding := requestedPasswordLength - len(input)
 
 	// If the input string is already longer than the desired length, return the input string
@@ -180,7 +227,14 @@ func randPadString(input string) string {
 	leftPadding := randomString(numPadding / 2)
 	rightPadding := randomString(numPadding - len(leftPadding))
 
-	// Concatenate the left and right padding with the input string and return the result
+	// Concatenate the left and right padding with the input string
 	output := leftPadding + input + rightPadding
+
+	// Generate a random position to insert the input string
+	rand.Seed(time.Now().UnixNano())
+	insertPos := rand.Intn(len(output) - len(input) + 1)
+
+	// Insert the input string at the random position and return the result
+	output = output[:insertPos] + input + output[insertPos+len(input):]
 	return output
 }
