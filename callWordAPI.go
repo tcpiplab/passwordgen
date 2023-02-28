d5f6afdc649bc38262754c3163c98a99294b7bcb
tcpiplab <lshep.usc@gmail.com>
2023-02-28 09:14:51 -0800

    add function to create a chain of random words

--- callWordAPI.go
+++ callWordAPI.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 type Words struct {
@@ -52,11 +55,6 @@ func callWordApi() string {
 		return ""
 	}
 
-	// Print the response data
-	//for _, word := range words {
-	//	fmt.Printf("Word: %s\nScore: %d\n\n", word.Word, word.Score)
-	//}
-
 	longestWord := ""
 	for _, word := range words {
 		if len(word.Word) > len(longestWord) {
@@ -64,7 +62,25 @@ func callWordApi() string {
 		}
 	}
 
-	//fmt.Println("Longest word:", longestWord)
-
 	return longestWord
+}
+
+func randomWordChain() string {
+	// Generate a random number between 2 and 5
+	rand.Seed(time.Now().UnixNano())
+	numWords := rand.Intn(4) + 2
+
+	// Call callWordApi() and concatenate the returned words into a string
+	var buffer bytes.Buffer
+	for i := 0; i < numWords; i++ {
+		word := callWordApi()
+		buffer.WriteString(word)
+		if i != numWords-1 {
+			// Add a delimiter between the words except for the last word
+			delimiters := "-_=+/\\|~^$#@&*:."
+			delimiter := string(delimiters[rand.Intn(len(delimiters))])
+			buffer.WriteString(delimiter)
+		}
+	}
+	return buffer.String()
 }
