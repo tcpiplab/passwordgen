1b7a5be48dfe0fe967d78957a3c14d9ec47e19c0
tcpiplab <lshep.usc@gmail.com>
2023-04-15 10:58:17 -0700

    Print example table is done

--- printUtils.go
+++ printUtils.go
@@ -632,8 +632,59 @@ func printPasswordTypesTable() []string {
 			CommandFlag:     "--mem1",
 		})
 
-	// TODO: STOPPED HERE: add the other six mem types
-	memorableTransformTwo(memorablePassword, requestedPasswordLength)
+	// Memorable Two example password
+	memorablePassword = ""
+	memorableTwoExample := memorableTransformTwo(memorablePassword, requestedPasswordLength)
+	arrayOfPasswordTypes = append(arrayOfPasswordTypes,
+		PasswordAndCommandFlag{
+			PasswordExample: memorableTwoExample,
+			CommandFlag:     "--mem2",
+		})
+
+	// Memorable Three example password
+	memorablePassword = ""
+	memorableThreeExample := memorableTransformThree(memorablePassword, requestedPasswordLength)
+	arrayOfPasswordTypes = append(arrayOfPasswordTypes,
+		PasswordAndCommandFlag{
+			PasswordExample: memorableThreeExample,
+			CommandFlag:     "--mem3",
+		})
+
+	// Memorable Four example password
+	memorablePassword = ""
+	memorableFourExample := memorableTransformFour(memorablePassword, requestedPasswordLength)
+	arrayOfPasswordTypes = append(arrayOfPasswordTypes,
+		PasswordAndCommandFlag{
+			PasswordExample: memorableFourExample,
+			CommandFlag:     "--mem4",
+		})
+
+	// Memorable Five example password
+	memorablePassword = ""
+	memorableFiveExample := memorableTransformFive(memorablePassword, requestedPasswordLength)
+	arrayOfPasswordTypes = append(arrayOfPasswordTypes,
+		PasswordAndCommandFlag{
+			PasswordExample: memorableFiveExample,
+			CommandFlag:     "--mem5",
+		})
+
+	// Memorable Six example password
+	memorablePassword = ""
+	memorableSixExample := memorableTransformSix(memorablePassword, requestedPasswordLength)
+	arrayOfPasswordTypes = append(arrayOfPasswordTypes,
+		PasswordAndCommandFlag{
+			PasswordExample: memorableSixExample,
+			CommandFlag:     "--mem6",
+		})
+
+	// Memorable Seven example password
+	memorablePassword = ""
+	memorableSevenExample := memorableTransformSeven(memorablePassword, requestedPasswordLength)
+	arrayOfPasswordTypes = append(arrayOfPasswordTypes,
+		PasswordAndCommandFlag{
+			PasswordExample: memorableSevenExample,
+			CommandFlag:     "--mem7",
+		})
 
 	// Print the slice of string pairs
 	for _, pair := range arrayOfPasswordTypes {
